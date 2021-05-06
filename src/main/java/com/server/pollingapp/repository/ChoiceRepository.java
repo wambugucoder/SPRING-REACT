4ffb2a7b539be2aea230acc7a7b94fd57cb2c415
JosWambugu <josphatwambugu77@gmail.com>
2021-05-06 10:10:11 +0300

    REMOVAL OF ADDITIONAL DATABASE QUERIES AND SHIFTING TO STREAMS

--- src/main/java/com/server/pollingapp/repository/ChoiceRepository.java
+++ src/main/java/com/server/pollingapp/repository/ChoiceRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ChoiceRepository extends JpaRepository<ChoiceModel,String> {
 
-    ChoiceModel findByOption(String option);
 }
