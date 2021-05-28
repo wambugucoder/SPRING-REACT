bef4576be841dceb0b1149052097d7386acfb0b1
Restyled.io <commits@restyled.io>
2021-05-28 08:15:25 +0000

    Restyled by prettier

--- client/src/constants/Constant.js
+++ client/src/constants/Constant.js
@@ -1,13 +1,11 @@
 export const API_BASE_URL =
-    'https://pollsapp-2021.westeurope.cloudapp.azure.com:8443/api/v1';
-export const ACCESS_TOKEN = 'jwtToken';
+  "https://pollsapp-2021.westeurope.cloudapp.azure.com:8443/api/v1";
+export const ACCESS_TOKEN = "jwtToken";
 
 export const OAUTH2_REDIRECT_URI =
-    'https://pollsapp-2021.westeurope.cloudapp.azure.com/oauth2/redirect'
+  "https://pollsapp-2021.westeurope.cloudapp.azure.com/oauth2/redirect";
 
 export const GOOGLE_AUTH_URL =
-    API_BASE_URL +
-    '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+  API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
 export const GITHUB_AUTH_URL =
-    API_BASE_URL +
-    '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
+  API_BASE_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
