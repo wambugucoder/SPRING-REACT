ac9a96adac199db5abe74f21fc4302d9b0615c97
JosWambugu <josphatwambugu77@gmail.com>
2021-05-28 11:36:09 +0300

    Config

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -11,7 +11,10 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    /*
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    */
+  
   )
 );
 
