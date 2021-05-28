3dd476dc5eda843022d7872b89bc5a67270659ce
JosWambugu <josphatwambugu77@gmail.com>
2021-05-28 11:34:49 +0300

    Configs

--- client/src/components/page-header-component/PageHeader.js
+++ client/src/components/page-header-component/PageHeader.js
@@ -1,5 +1,5 @@
 import { Link, useHistory } from "react-router-dom"
-import { PageHeader, Menu,Avatar,Image } from 'antd';
+import { PageHeader, Menu,Avatar,Image, Space } from 'antd';
 import { MailOutlined,HomeOutlined,EditOutlined, UserOutlined,CalendarOutlined, LogoutOutlined } from '@ant-design/icons';
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -61,12 +61,12 @@ const RenderPageHeader=()=>{
           </Menu.Item>
           <SubMenu key="SubMenu"  title={<RenderAvatar/>}>
           <Menu.ItemGroup style={{fontWeight:'bold'}} title="Profile">
-          <Menu.Item key="setting:1"><UserOutlined/>{auth.user.UserName}</Menu.Item>
-          <Menu.Item key="setting:2"><MailOutlined/>{auth.user.Email}</Menu.Item>
-          <Menu.Item key="setting:3"><CalendarOutlined/>{createdAt}</Menu.Item>
+          <Menu.Item key="setting:1"><UserOutlined/><Space/>{auth.user.UserName}</Menu.Item>
+          <Menu.Item key="setting:2"><MailOutlined/><Space/>{auth.user.Email}</Menu.Item>
+          <Menu.Item key="setting:3"><CalendarOutlined/><Space/>{createdAt}</Menu.Item>
           </Menu.ItemGroup>
           <Menu.ItemGroup style={{fontWeight:'bold'}} title="Logout">
-          <Menu.Item key="setting:4" onClick={()=>{dispatch(LogOutUser())}}><LogoutOutlined/>Exit</Menu.Item>
+          <Menu.Item key="setting:4" onClick={()=>{dispatch(LogOutUser())}}><LogoutOutlined/><Space/>Exit</Menu.Item>
            </Menu.ItemGroup>
           </SubMenu>
           </Menu>,
